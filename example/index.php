1a9fea6ba02911113fc00a326b41e68ddd0242f2
Guillermo de la Iglesia <zguillez@gmail.com>
2017-05-10 17:07:40 +0200

    json get

--- example/index.php
+++ example/index.php
@@ -2,5 +2,9 @@
   require 'vendor/autoload.php';
   use Z\Tools;
 
-  $tools = new Tools();
-  $tools->test('This is a test!');
+  $tools  = new Tools();
+  $data   = ['nombre' => 'nombre', 'apellidos' => 'apellidos', 'provincia' => '28', 'cp' => '28914',
+             'domicilio' => 'domicilio', 'telefono' => '612312312', 'email' => 'prueba@prueba.com',
+             'nivelestudios' => '1', 'edad' => '34', 'idcurso' => '105', 'pais' => '90', 'idcampana' => '1135'];
+  $result = $tools->get('http://tracker.masterd.es/json', $data, true);
+  var_dump($result);
--- src/Tools.php
+++ src/Tools.php
@@ -11,8 +11,29 @@
     }
     public function test($data)
     {
-      var_dump($this->date);
-      var_dump($this->ip);
-      var_dump($data);
+      var_dump([$this->date, $this->ip, $data]);
+    }
+    public function get($url, $data = null, $isJson = false)
+    {
+      if($data) {
+        foreach($data as $item => $value) {
+          if(strpos($url, '?') === false) {
+            $url .= '?' . $item . '=' . urlencode($value);
+          } else {
+            $url .= '&' . $item . '=' . urlencode($value);
+          }
+        }
+      }
+      $handler = curl_init();
+      curl_setopt($handler, CURLOPT_URL, $url);
+      curl_setopt($handler, CURLOPT_HEADER, false);
+      curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+      $response = curl_exec($handler);
+      curl_close($handler);
+      if($isJson) {
+        $response = json_decode($response, true);
+      }
+
+      return $response;
     }
   }
