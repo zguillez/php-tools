9fe62f5cda905b902ed2c5a434d6bb0bc89a0979
Guillermo de la Iglesia <zguillez@gmail.com>
2017-05-11 16:46:34 +0200

    csv & excel download

--- example/index.php
+++ example/index.php
@@ -3,9 +3,11 @@
   use Z\Tools;
 
   $tools = new Tools();
-  $data  = ['nombre' => 'nombre', 'apellidos' => 'apellidos', 'provincia' => '28', 'cp' => '28914',
+  /*$data  = ['nombre' => 'nombre', 'apellidos' => 'apellidos', 'provincia' => '28', 'cp' => '28914',
             'domicilio' => 'domicilio', 'telefono' => '612312312', 'email' => 'prueba@prueba.com',
             'nivelestudios' => '1', 'edad' => '34', 'idcurso' => '105', 'pais' => '90', 'idcampana' => '1135'];
   //$result = $tools->get('http://tracker.masterd.es/json', $data, true);
   $result = $tools->post('http://tracker.masterd.es/json', $data, true);
-  var_dump($result);
+  var_dump($result);*/
+  $data = [[1, "a"], [2, "b"], [3, "c"], [4, "d"]];
+  $tools->excel('test', $data, ['id', 'value']);
--- src/Tools.php
+++ src/Tools.php
@@ -4,6 +4,7 @@
   {
     private $date;
     private $ip;
+    private $conn;
     public function __construct()
     {
       $this->date = date("Y-m-d H:i:s");
@@ -13,6 +14,9 @@
     {
       var_dump([$this->date, $this->ip, $data]);
     }
+    /**
+     * HTTP
+     */
     public function get($url, $data = null, $isJson = false)
     {
       if($data) {
@@ -58,5 +62,85 @@
       }
 
       return $response;
+    }
+    /**
+     * DATABASE
+     */
+    public function database($servername, $username, $password, $dbname)
+    {
+      $this->conn = new mysqli($servername, $username, $password, $dbname);
+      if($this->conn->connect_error) {
+        die("Connection failed: " . $this->conn->connect_error);
+      }
+
+      return $this->conn;
+    }
+    public function sql($sql)
+    {
+      return $this->conn->query($sql);
+    }
+    public function sql2csv($filename, $sql, $schema = null)
+    {
+      $result = $this->conn->query($sql);
+      $data   = [];
+      if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          $data[] = $row;
+        }
+      }
+      $this->csv($filename, $data, $schema);
+    }
+    /**
+     * FILES
+     */
+    public function csv($filename, $data = null, $schema = null)
+    {
+      header('Content-Type: text/csv; charset=utf-8');
+      header('Content-Disposition: attachment; filename=' . $filename . '.csv');
+      $output = fopen('php://output', 'w');
+      if($schema) {
+        fputcsv($output, $schema);
+      }
+      if($data) {
+        foreach($data as $row) {
+          fputcsv($output, $row);
+        }
+      }
+    }
+    public function excel($filename, $data = null, $schema = null)
+    {
+      header('Content-Type: application/vnd.ms-excel; charset=utf-8');
+      header('Content-Disposition: attachment; filename=' . $filename . '.xlsx');
+      for($i = 65; $i <= 90; $i ++) {
+        $letras[] = chr($i);
+      }
+      $total = count($data);
+      for($i = 0; $i < $total; $i ++) {
+        if($i < count($letras)) {
+          $cells[] = $letras[$i];
+        } else {
+          $pag     = floor($i / count($letras));
+          $letra   = $i - $pag * count($letras);
+          $cells[] = $letras[$pag - 1] . $letras[$letra];
+        }
+      }
+      $doc = new \PHPExcel();
+      $doc->setActiveSheetIndex(0);
+      if($schema) {
+        for($i = 0; $i < count($schema); $i ++) {
+          $doc->getActiveSheet()->SetCellValue($cells[$i] . '1', $schema[$i]);
+        }
+        $index = 2;
+      } else {
+        $index = 1;
+      }
+      foreach($data as $row) {
+        for($i = 0; $i < count($row); $i ++) {
+          $doc->getActiveSheet()->SetCellValue($cells[$i] . $index, $row[$i]);
+        }
+        $index ++;
+      }
+      $objWriter = \PHPExcel_IOFactory::createWriter($doc, 'Excel2007');
+      $objWriter->save('php://output');
     }
   }
